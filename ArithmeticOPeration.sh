bfad637a800eaa1997f34d5ee0cf4492aa29193c
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-25 12:39:31 +0530

    Store result Dictionary

--- ArithmeticOPeration.sh
+++ ArithmeticOPeration.sh
@@ -20,7 +20,6 @@ resDict["k2"]= `awk 'BEGIN{printf("%0.2f", '$num1' * '$num2' + '$num3' )}'`;
 resDict["k3"]= `awk 'BEGIN{printf("%0.2f", '$num1' + '$num2' / '$num3' )}'`;
 resDict["k4"]= `awk 'BEGIN{printf("%0.2f", '$num1' % '$num2' + '$num3' )}'`;
 
-echo "computation result is:" ${resDict[k1]}
-echo "keys: " ${!resDict[@]}
+echo "result is:" ${resDict[@]}
 
-
+echo "keys" ${resDict[@]}
