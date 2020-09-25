6daa9274539cf3d0582a090be8873ffa3e2f1f56
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-25 12:26:31 +0530

    Compute Fourth operation

--- ArithmeticOPeration.sh
+++ ArithmeticOPeration.sh
@@ -18,6 +18,7 @@ echo $num1 $num2 $num3
 resDict["k1"]= `awk 'BEGIN{printf("%0.2f", '$num1' + '$num2' * '$num3' )}'`;
 resDict["k2"]= `awk 'BEGIN{printf("%0.2f", '$num1' * '$num2' + '$num3' )}'`;
 resDict["k3"]= `awk 'BEGIN{printf("%0.2f", '$num1' + '$num2' / '$num3' )}'`;
+resDict["k4"]= `awk 'BEGIN{printf("%0.2f", '$num1' % '$num2' + '$num3' )}'`;
 
 echo "computation result is:" ${resDict[k1]}
 echo "keys: " ${!resDict[@]}
