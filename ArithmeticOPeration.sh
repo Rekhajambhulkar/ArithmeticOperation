c85abe212931d44410e6ca5bdbc93871cd7dafea
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-25 13:25:03 +0530

    Ascending order

--- ArithmeticOPeration.sh
+++ ArithmeticOPeration.sh
@@ -15,18 +15,22 @@ read num3
 
 echo $num1 $num2 $num3
 
+#compute the arithmetic Operations
 Dict[k1] = `awk 'BEGIN{printf("%0.2f", '$num1' + '$num2' * '$num3' )}'`;
 Dict[k2] = `awk 'BEGIN{printf("%0.2f", '$num1' * '$num2' + '$num3' )}'`;
 Dict[k3] = `awk 'BEGIN{printf("%0.2f", '$num1' + '$num2' / '$num3' )}'`;
 Dict[k4] = `awk 'BEGIN{printf("%0.2f", '$num1' % '$num2' + '$num3' )}'`;
 
+#Store the res into the Dictionary
 echo "result is:" ${Dict[@]}
 
-
+#Reav values from Dictionary to array
 Array=${Dict[@]}
 
+#Sort the result in Descending order
 DescendingRes=$(for i in ${Array[@]};do echo $i;done | sort -nr);
 echo "res in descending order:" $DescendingRes
 
+#Sort the result in Ascending order
 AscendingRes=$(for i in ${Array[@]};do echo $i;done | sort -n);
 echo "Res in Ascending oder:" $AscendingRes
