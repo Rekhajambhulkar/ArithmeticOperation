5350754255a01368fc126c42f2677ef48f44cfe1
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-25 11:28:21 +0530

    Input from User

--- ArithmeticOPeration.sh
+++ ArithmeticOPeration.sh
@@ -1,5 +1,14 @@
 #! /bin/bash -x
 
+
 echo "Welcome in ArithmeticOperation"
 
+#Take input from User
+echo "Enter 1st no:" 
+read num1 
+echo "Enter 2nd no:" 
+read num2
+echo "Enter 3rd no:"
+read num3
 
+echo $num1 $num2 $num3
